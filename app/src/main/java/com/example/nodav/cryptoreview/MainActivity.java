01106a861b12ab27dc83ebaae5ec5f54ad08af58
VladyslavNoda <grossghost13@gmail.com>
2017-12-28 17:21:53 +0200

    add controll versions

--- app/src/main/java/com/example/nodav/cryptoreview/MainActivity.java
+++ app/src/main/java/com/example/nodav/cryptoreview/MainActivity.java
@@ -5,9 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.nodav.cryptoreview.network.RestManager;
+import com.github.javiersantos.appupdater.AppUpdater;
+import com.github.javiersantos.appupdater.AppUpdaterUtils;
+import com.github.javiersantos.appupdater.enums.AppUpdaterError;
+import com.github.javiersantos.appupdater.enums.Display;
+import com.github.javiersantos.appupdater.enums.UpdateFrom;
+import com.github.javiersantos.appupdater.objects.Update;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +43,33 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
+        new AppUpdater(this)
+                .setDisplay(Display.SNACKBAR)
+                .setDisplay(Display.DIALOG)
+                .setDisplay(Display.NOTIFICATION)
+                .setUpdateFrom(UpdateFrom.GITHUB)
+                .setGitHubUserAndRepo("GrossGhost", "CryptoReview")
+                .start();
+
+//        AppUpdaterUtils appUpdaterUtils = new AppUpdaterUtils(this)
+//                .setUpdateFrom(UpdateFrom.GITHUB)
+//                .setGitHubUserAndRepo("GrossGhost", "CryptoReview")
+//                .withListener(new AppUpdaterUtils.UpdateListener() {
+//                    @Override
+//                    public void onSuccess(Update update, Boolean isUpdateAvailable) {
+//                        Log.d("Latest Version", update.getLatestVersion());
+//                        Log.d("Latest Version Code", update.getLatestVersionCode() + "");
+//                        Log.d("Release notes", update.getReleaseNotes());
+//                        Log.d("URL", update.getUrlToDownload()+ "");
+//                        Log.d("Is update available?", Boolean.toString(isUpdateAvailable));
+//                    }
+//
+//                    @Override
+//                    public void onFailed(AppUpdaterError error) {
+//                        Log.d("AppUpdater Error", "Something went wrong");
+//                    }
+//                });
+//        appUpdaterUtils.start();
     }
 
 }
